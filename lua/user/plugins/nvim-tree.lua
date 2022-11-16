bdba040412aa0178b3e1d19cc3d1ee50f3662b98
Leander Perera <ltcperera@gmail.com>
2022-11-15 21:07:55 -0800

    Added nvim-tree customization to use h,j,k,l keys for navigation

--- lua/user/plugins/nvim-tree.lua
+++ lua/user/plugins/nvim-tree.lua
@@ -11,6 +11,17 @@ vim.cmd([[ highlight NvimTreeIndentMarker guifg=#3FC5FF ]])
 
 -- configure nvim-tree
 nvimtree.setup({
+  view = {
+    mappings = {
+      custom_only = false,
+        list = {
+          { key = "l", action = "edit", action_cb = edit_or_open },
+          { key = "L", action = "vsplit_preview", action_cb = vsplit_preview },
+          { key = "h", action = "close_node" },
+          { key = "H", action = "collapse_all", action_cb = collapse_all }
+        }
+    },
+  },
   -- change folder arrow icons
   renderer = {
     icons = {
@@ -24,9 +35,11 @@ nvimtree.setup({
   },
   actions = {
     open_file = {
+      quit_on_open = false,
       window_picker = {
         enable = false,
       },
     },
   },
 })
+
