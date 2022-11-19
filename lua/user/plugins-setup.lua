8b22ea463579772663299b1b07d46288dae7ebcb
Leander Perera <ltcperera@gmail.com>
2022-11-19 09:14:30 -0800

    Added vim-tmux-navigator plugin to use tmux keys to navigate between window panes.

--- lua/user/plugins-setup.lua
+++ lua/user/plugins-setup.lua
@@ -36,6 +36,8 @@ return packer.startup(function(use)
 
   use("bluz71/vim-nightfly-guicolors") -- preferred colorscheme
 
+  use("christoomey/vim-tmux-navigator") -- tmux & split window navigation
+
   -- commenting with gc
   use("numToStr/Comment.nvim")
 
